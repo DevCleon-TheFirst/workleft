a428ceb2a7ff58c87a6070921fa0c7af7ed8220f
DevCleon <empireseeker>
2026-08-31 10:32:53 +0200

    Fix: clean up app.blade.php, remove debug error catcher

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -23,35 +23,12 @@
     </head>
     <body class="antialiased" style="background-color:#0a0a0f; color:#e2e8f0;">
         @inertia
-        
+
         <script>
-            // === ERROR CATCHER - REMOVE AFTER DEBUGGING ===
-            window.onerror = function(msg, src, line, col, error) {
-                var div = document.getElementById('js-error-box');
-                if (!div) {
-                    div = document.createElement('div');
-                    div.id = 'js-error-box';
-                    div.style.cssText = 'position:fixed;top:0;left:0;right:0;background:#ff0000;color:#fff;padding:20px;z-index:99999;font-size:14px;font-family:monospace;word-break:break-all;';
-                    document.body.appendChild(div);
-                }
-                div.innerHTML += '<b>JS ERROR:</b> ' + msg + '<br><b>File:</b> ' + src + '<br><b>Line:</b> ' + line + '<br><br>';
-                return false;
-            };
-            window.addEventListener('unhandledrejection', function(event) {
-                var div = document.getElementById('js-error-box');
-                if (!div) {
-                    div = document.createElement('div');
-                    div.id = 'js-error-box';
-                    div.style.cssText = 'position:fixed;top:0;left:0;right:0;background:#ff0000;color:#fff;padding:20px;z-index:99999;font-size:14px;font-family:monospace;word-break:break-all;';
-                    document.body.appendChild(div);
-                }
-                div.innerHTML += '<b>PROMISE ERROR:</b> ' + event.reason + '<br><br>';
-            });
-        </script>
             if ('serviceWorker' in navigator) {
                 window.addEventListener('load', () => {
                     navigator.serviceWorker.register('/sw.js').then(registration => {
-                        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+                        console.log('ServiceWorker registered with scope: ', registration.scope);
                     }).catch(err => {
                         console.log('ServiceWorker registration failed: ', err);
                     });
